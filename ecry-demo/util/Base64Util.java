8516024c15bd587b448fcc0905bb81db88057ec9
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-11-10 02:10:57 +0800

    加密算法工具类

---
+++ ecry-demo/util/Base64Util.java
@@ -0,0 +1,33 @@
+package com.encry.util;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * @version 6.1.8
+ * @author: Abraham Vong
+ * @date: 2021.9.19
+ * @GitHub https://github.com/AbrahamTemple/
+ * @description:
+ */
+public class Base64Util {
+
+    /**
+     * 编码字符串
+     * @param str
+     * @return
+     */
+    public static String encode(String str){
+        if(StringUtil.isEmpty(str)) return "";
+        return Base64.encodeBase64String(str.getBytes());
+    }
+
+    /**
+     * 解码字符串
+     * @param str
+     * @return
+     */
+    public static String decode(String str){
+        if(StringUtil.isEmpty(str)) return "";
+        return new String(Base64.decodeBase64(str));
+    }
+}
