8214169e6d78fab70c57d7951ac5d5a2f197b0b5
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-11-30 03:02:41 +0800

    静态资源

---
+++ chat-vue/public/assets/js/script.js
@@ -0,0 +1,34 @@
+const sideMenuWrapper = document.querySelector('.mobile-menu-wrp');
+const sideMenu = document.querySelector('.mobile-menu');
+var menuButton = document.getElementById("menu-btn");
+
+menuButton.addEventListener('click', function(){
+    sideMenuWrapper.style.display = 'block';
+    sideMenuWrapper.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    setTimeout(function () {
+        sideMenu.style.transform = 'translateX(440px)';
+    }, 10);
+})
+
+sideMenuWrapper.addEventListener('click', function(){
+    sideMenu.style.transform = 'translateX(0px)';
+    sideMenuWrapper.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    setTimeout(function () {
+        sideMenuWrapper.style.display = 'none';
+    }, 500);
+})
+
+// window.addEventListener('click', () => {
+//     let target = event.target;
+//     if (target.className === 'menu-button') {
+		
+        
+//     } else if (target.className === 'mobile-menu-wrp') {
+// 		console.log('menu-button');
+//         sideMenu.style.transform = 'translateX(0px)';
+//         sideMenuWrapper.style.backgroundColor = "rgba(0, 0, 0, 0)";
+//         setTimeout(function () {
+//             sideMenuWrapper.style.display = 'none';
+//         }, 500);
+//     }
+// });
